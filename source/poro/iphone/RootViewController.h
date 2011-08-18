63816015202c14547a5e5265534a6e00c932ac23
grapefrukt <grapefrukt@grapefrukt.com>
2011-08-18 17:45:12 +0800

    Added toggles for auto rotation on iOS

--- source/poro/iphone/RootViewController.h
+++ source/poro/iphone/RootViewController.h
@@ -10,17 +10,20 @@
 #define INC_ROOTVIEWCONTROLLER_H
 
 #import <UIKit/UIKit.h>
-#import "EAGLView.h";
+#import "EAGLView.h"
 #import "platform_iphone.h"
 #import "globals_iphone.h"
 
 @interface RootViewController : UIViewController {
-	UIInterfaceOrientation deviceOrientation_;
 	bool mMatrixLoaded;
 }
 
 -(void) setDeviceOrientation:(UIInterfaceOrientation) orientation;
--(void) applyOrientation;
+-(void) applyOrientation: (poro::PlatformIPhone::DEVICE_ORIENTATION) newOrientation;
+
+-(poro::PlatformIPhone::DEVICE_ORIENTATION) convertOrientationIOsToPoro:(UIInterfaceOrientation) orientation;
+-(UIInterfaceOrientation) convertOrientationPoroToIOs:(poro::PlatformIPhone::DEVICE_ORIENTATION) orientation;
+
 
 @end
 
