587954d37a64e1fa9cce69c95acd9ca50467e73d
Petri Purho <petri.purho@gmail.com>
2011-10-07 08:49:41 +0300

    Config_macro.h is now a bit more fleshed out (including += operator)

--- source/utils/config_macro/config_macro.h
+++ source/utils/config_macro/config_macro.h
@@ -18,7 +18,6 @@
  *
  ***************************************************************************/
 
-
 ///////////////////////////////////////////////////////////////////////////////
 //
 // config_macro.h
@@ -67,17 +66,30 @@
 #ifndef INC_CONFIG_MACRO_H
 #define INC_CONFIG_MACRO_H
 
+#include <map>
+#include <string>
+#include "../xml/canycontainer.h"
+
 namespace ceng {
 
 #define CONFIG_HEADER_DECLARE(type, name, value) \
     type name;
 
 #define CONFIG_XML_BIND_DECLARE(type, name, value) \
-	XML_BindAttribute( filesys, name );
+	XML_BindAttribute( filesys, name ); \
+	if( filesys->GetNode()->HasAttribute( #name ) ) _m_variables[ #name ] = true; 
 
 #define CONFIG_INIT_MEMBERS(type, name, value) \
 	name = value;
 
+#define CONFIG_SET_OTHER(type, name, value) \
+	name = other.name; 
+
+#define CONFIG_SET_VALUE(type, name, value) \
+	if( n == #name ) { name = ceng::CAnyContainerCast< type >( new_value ); return; }
+
+#define CONFIG_GET_VALUE(type, name, value) \
+	if( n == #name ) return name;
 
 #define DEFINE_CONFIG(name, list)                 \
     class name { \
@@ -86,11 +98,41 @@ namespace ceng {
 			list(CONFIG_INIT_MEMBERS) \
 		} \
 		\
+		name( const name& other ) { Set( other ); } \
+		\
+		const name& operator= ( const name& other ) { Set( other ); return *this; } \
+		\
+		const name& operator+= ( const name& other ) \
+		{ \
+			for( std::map< std::string, bool >::const_iterator i = other._m_variables.begin(); i != other._m_variables.end(); ++i ) { \
+				if( i->second ) SetValue( i->first, other.GetValue( i->first ) ); \
+			} \
+			return *this; \
+		} \
+		\
         list(CONFIG_HEADER_DECLARE)   \
+		std::map< std::string, bool > _m_variables; \
 		\
 		void Serialize( ceng::CXmlFileSys* filesys ) \
 		{ \
 			list(CONFIG_XML_BIND_DECLARE) \
+		} \
+		\
+		void Set( const name& other ) \
+		{ \
+			list(CONFIG_SET_OTHER) \
+			_m_variables = other._m_variables; \
+		} \
+		\
+		void SetValue( const std::string& n, const ceng::CAnyContainer& new_value ) \
+		{ \
+			list(CONFIG_SET_VALUE) \
+		} \
+		\
+		ceng::CAnyContainer GetValue( const std::string& n ) const \
+		{ \
+			list(CONFIG_GET_VALUE) \
+			return ceng::CAnyContainer(); \
 		} \
 	};
 
