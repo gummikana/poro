99d00a8336bb64f126876e7c476f6564ffee3848
Petri Purho <petri.purho@gmail.com>
2011-10-07 09:08:23 +0300

    Particles now use actionscript sprite instead of the csprite

--- source/game_utils/particle/cparticlefactory.cpp
+++ source/game_utils/particle/cparticlefactory.cpp
@@ -1,24 +1,3 @@
-/***************************************************************************
- *
- * Copyright (c) 2009 - 2011 Petri Purho, Dennis Belfrage
- *
- * This software is provided 'as-is', without any express or implied
- * warranty.  In no event will the authors be held liable for any damages
- * arising from the use of this software.
- * Permission is granted to anyone to use this software for any purpose,
- * including commercial applications, and to alter it and redistribute it
- * freely, subject to the following restrictions:
- * 1. The origin of this software must not be misrepresented; you must not
- *    claim that you wrote the original software. If you use this software
- *    in a product, an acknowledgment in the product documentation would be
- *    appreciated but is not required.
- * 2. Altered source versions must be plainly marked as such, and must not be
- *    misrepresented as being the original software.
- * 3. This notice may not be removed or altered from any source distribution.
- *
- ***************************************************************************/
-
-
 /*
  *  cparticlesfactory.cpp
  *  cpt-farbs
@@ -29,6 +8,16 @@
  */
 
 #include "cparticlefactory.h"
+
+CParticleFactory::CParticleFactory() :
+	myParticles()
+{
+}
+
+CParticleFactory::~CParticleFactory()
+{
+	Clear();
+}
 
 CParticle* CParticleFactory::NewParticle( CSprite* sprite, float life_time, const types::vector2& velocity, float rotation )
 {
@@ -57,16 +46,6 @@ void CParticleFactory::Update( float dt )
 		{
 			++i;
 		}
-	}
-	
-}
-
-void CParticleFactory::Draw(poro::IGraphics* graphics)
-{
-	
-	for( unsigned int i = 0; i < myParticles.size(); ++i)
-	{
-		myParticles[ i ]->Draw(graphics);
 	}
 	
 }
