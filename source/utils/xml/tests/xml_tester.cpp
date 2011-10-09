97c0addc675d581a68f82cccb2559d1f1d44b762
Petri Purho <petri.purho@gmail.com>
2011-10-09 16:40:21 +0300

    Fixed the function ptr implementation for IOS, fixed the xml tests so that they are more sane

--- source/utils/xml/tests/xml_tester.cpp
+++ source/utils/xml/tests/xml_tester.cpp
@@ -53,63 +53,33 @@ public:
 
 int CXmlTest()
 {
-	// Just to fuck up the xml
+	// Just to mess up the xml
 	{
 		CXmlTester load;
 		XmlLoadFromFile( load, "temp/test_sprite.xml", "Sprite" );
 		
 	}
 
+	
+	CXmlNode* test_node = NULL;
 	{
 		CXmlTester test;
 		test.integer = 5;
-		XmlSaveToFile( test, "temp/test_xml.xml", "Sprite" );
+		// XmlSaveToFile( test, "temp/test_xml.xml", "Sprite" );
+		test_node = XmlSaveToMemory( test, "Sprite" );
+		test_assert( test_node );
 	}
 
 	{
 		CXmlTester test;
-		XmlLoadFromFile( test, "temp/test_xml.xml", "Sprite" );
+		// XmlLoadFromFile( test, "temp/test_xml.xml", "Sprite" );
+		XmlLoadFromMemory( test, test_node, "Sprite" );
 		test_assert( test.integer == 5 );
 	}		
 
-	{
-		CXmlTester test;
-		XmlLoadFromFile( test, "temp/test_xml.xml", "gprite" );
-	}
-/*	CXmlTester save;
+	delete test_node;
+	test_node = NULL;
 
-	save.integer = 1234;
-	// save.pointer = &save.integer;
-	save.vector.push_back( 1 );
-	save.vector.push_back( 2 );
-	save.vector.push_back( 3 );
-
-	save.list.push_back( "foo" );
-	save.list.push_back( "bar" );
-
-	logger << "Saving CXmlTester class to test.xml" << std::endl;
-	XmlSaveToFile( save, "../src/ceng/utils/xml/tests/test.xml", "CXmlTester" );
-	logger << "Saving done properly\n" << std::endl;
-
-	CXmlTester load;
-
-	logger << "Loading CXmlTester class from test.xml" << std::endl;
-	XmlLoadFromFile( load, "../src/ceng/utils/xml/tests/test.xml", "CXmlTester" );
-	logger << "Loading done properly\n" << std::endl;
-
-	test_assert( load.integer == 1234 );
-	// test_assert( *load.pointer == 1234 );
-	test_assert( load.vector.size() == 3 );
-	test_assert( load.list.size() == 2 );
-	test_assert( load.vector[1] == 2 );
-	test_assert( *load.list.begin() == "foo" );
-	
-	logger << "CXmlTester was excecuted with out any problems" << std::endl;
-
-	// logger << "Saving CXmlTester class to test.xml" << std::endl;
-	// XmlSaveToFile( test, "../src/ceng/utils/xml/tests/test.xml", "CXmlTester" );
-	// logger << "Saving done properly\n" << std::endl;
-*/
 	return 0;
 }
 
