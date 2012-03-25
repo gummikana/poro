fcb11fbcf19cc1c6881919a7e396a5494e7627a8
Petri Purho <petri.purho@gmail.com>
2012-03-25 04:43:02 +0300

    Added a test to infinitegrid

---
+++ source/utils/infinitegrid/tests/cgridpoint_test.cpp
@@ -0,0 +1,80 @@
+#include "../cgridpoint.h"
+#include "../../debug.h"
+#include <memory>
+
+//---------------------------------------------------------------------------------------------
+
+#ifdef CENG_TESTER_ENABLED
+
+namespace ceng {
+namespace test {
+
+int CGridPoint_Test()
+{
+	{
+		CGridPoint< int > test1( 10, 20 );
+		CGridPoint< int > test2;
+	
+
+		test_assert( test1.GetX() == 10 );
+		test_assert( test1.GetY() == 20 );
+
+		test_assert( test1 != test2 );
+		test2 = test1;
+		test_assert( test1 == test2 );
+		test_assert( ! ( test1 < test2 ) );
+		test_assert( ( test1 <= test2 ) );
+		test_assert( ! ( test1 > test2 ) );
+		test_assert( ( test1 >= test2 ) );
+
+
+		test1.SetX( 12 );
+		test_assert( test1.GetX() == 12 );
+		test_assert( test1.GetY() == 20 );
+
+		test1.SetY( 22 );
+		test_assert( test1.GetX() == 12 );
+		test_assert( test1.GetY() == 22 );
+
+		test1.Set( 1, 2 );
+		test_assert( test1.GetX() == 1 );
+		test_assert( test1.GetY() == 2 );
+	} 
+
+
+	{
+		CGridPoint< int > test1( 10, 20 );
+		CGridPoint< int > test2;
+	
+
+		test_assert( test1.GetX() == 10 );
+		test_assert( test1.GetY() == 20 );
+
+		test_assert( test1 != test2 );
+		test2 = test1;
+		test_assert( test1 == test2 );
+
+
+		test1.SetX( 12 );
+		test_assert( test1.GetX() == 12 );
+		test_assert( test1.GetY() == 20 );
+
+		test1.SetY( 22 );
+		test_assert( test1.GetX() == 12 );
+		test_assert( test1.GetY() == 22 );
+
+		test1.Set( 1, 2 );
+		test_assert( test1.GetX() == 1 );
+		test_assert( test1.GetY() == 2 );
+	}
+
+
+	return 0;
+}
+
+TEST_REGISTER( CGridPoint_Test );
+
+} // end of namespace test 
+} // end of namespace ceng
+
+#endif
