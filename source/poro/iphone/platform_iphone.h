d5296d8edb7e30227ef40a7c598c07955b228414
grapefrukt <grapefrukt@grapefrukt.com>
2012-04-29 12:15:48 +0300

    PlatformIphone header file lacked the declaration for GetRandomSeed. It's fixed now

--- source/poro/iphone/platform_iphone.h
+++ source/poro/iphone/platform_iphone.h
@@ -83,6 +83,8 @@ public:
 	void SetDeviceOrientation( int orientation ) { mDeviceOrientation =orientation; }
 	int GetDeviceOrientation() const { return mDeviceOrientation; }
 	
+	int GetRandomSeed();
+	
 protected:
 	GraphicsOpenGLES *mGraphics;
 	int mFrameCount;
