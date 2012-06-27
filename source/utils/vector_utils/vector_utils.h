9d778c28f65c976080c07eae2b9147871155c597
gummikana <petri.purho@gmail.com>
2012-06-28 02:42:00 +0300

    Added vector_utils.h to /utils/ - includes a bunch of vector utility functions that are used all the time

---
+++ source/utils/vector_utils/vector_utils.h
@@ -0,0 +1,72 @@
+#ifndef INC_VECTOR_UTILS_H
+#define INC_VECTOR_UTILS_H
+
+#include <vector>
+#include <string>
+
+namespace ceng {
+
+
+// adds an element to the vector if it doesn't exist in the container
+// returns true if the element was added
+// returns false if the element was in the container already
+template< class T >
+bool VectorAddUnique( const T& element, std::vector< T >& container )
+{
+	for( std::size_t i = 0; i < container.size(); ++i ) {
+		if( container[ i ] == element ) return false;
+	}
+
+	container.push_back( element );
+	return true;
+}
+
+//-----------------------------------------------------------------------------
+//
+// Serializing a vector of pointers 
+// this one is like one of the most used things I have reimplemented everytime
+// now it's in a nice templated class
+//
+template< class T >
+struct VectorXmlSerializer
+{
+	VectorXmlSerializer( std::vector< T* >& array, const std::string& name ) : array( array ), name( name ) { }
+
+	void Serialize( ceng::CXmlFileSys* filesys )
+	{
+		if( filesys->IsWriting() )
+		{
+			for( std::size_t i = 0; i < array.size(); ++i )
+			{
+				if( array[ i ] )
+					XML_BindAlias( filesys, *(array[i]), name );
+			}
+		}
+		else if ( filesys->IsReading() )
+		{
+			// clears the pointers
+			for( std::size_t i = 0; i < array.size(); ++i )
+				delete array[ i ];
+
+			array.clear();
+			int i = 0;
+			for( i = 0; i < filesys->GetNode()->GetChildCount(); i++ )
+			{
+				if( filesys->GetNode()->GetChild( i )->GetName() == name )
+				{
+					T* temp = new T;
+					ceng::XmlConvertTo( filesys->GetNode()->GetChild( i ), *temp );
+					array.push_back( temp );
+				}
+			}
+		}
+	}
+
+	std::vector< T* >& array;
+	std::string name;
+};
+//-----------------------------------------------------------------------------
+
+} //end of namespace ceng
+
+#endif
