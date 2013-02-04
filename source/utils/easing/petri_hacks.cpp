c0396bb66a01f1992fbb78d840080af928e7bcda
Petri Purho <petri.purho@gmail.com>
2013-02-04 11:18:37 +0200

    Added few easing functions into utils/easing/petri_hacks

--- source/utils/easing/petri_hacks.cpp
+++ source/utils/easing/petri_hacks.cpp
@@ -11,6 +11,12 @@ namespace easing {
 
 float PetriHacks::SinGoTo2AndBack::f( float x ) { return sin(x*3.14f)*2.f; }
 
+float PetriHacks::LittleBackAndForth::f( float x ) { return ((pow(x+0.17f, 1.3f) * sin(1.f/(x+0.17f))) * 1.043f) + 0.035f; }
+
+float PetriHacks::DimishingShakeBack::f( float x ) { return ( ( cos( x * 3.1415f ) + 1 ) * 0.5f )  * sin( x * 30.0f ); }
+
+float PetriHacks::DimishingShake::f( float x ) { return ( ( cos( x * 3.1415 ) + 1 ) * 0.5 )  * sin( x * 30.0 ) + x; }
+
 // Randomf, just gives you a ceng::math::Randomf(0,1) as a result
 float PetriHacks:: Randomf::f( float x ) { return ceng::Randomf( 0.f, 1.f ); }
 
@@ -19,6 +25,9 @@ float PetriHacks:: BooleanRandom::f( float x ) { return (float)ceng::Random( 0, 
 
 
 PetriHacks::SinGoTo2AndBack		PetriHacks::sinGoTo2AndBack;
+PetriHacks::DimishingShakeBack	dimishingShakeBack;
+PetriHacks::DimishingShakeBack	dimishingShake;
+PetriHacks::LittleBackAndForth	littleBackAndForth;
 PetriHacks::Randomf				PetriHacks::randomf;
 PetriHacks::BooleanRandom		PetriHacks::booleanRandom;
 
--- source/utils/easing/petri_hacks.h
+++ source/utils/easing/petri_hacks.h
@@ -12,12 +12,40 @@ class PetriHacks {
 	
 	public:
 
+		// Back functions are functions that return to 0,
+		// Why? well sometimes you need things to return nicely as well
+
 		// x goes to 2 and then down to 0
 		// good for in and out style effects. 
 		// Want something to drop and then dissapper... use this baby
+		// sin(x*3.14f)*2.f
 		struct SinGoTo2AndBack : public IEasingFunc {
 			virtual float f( float x );
 		};
+
+		// Shakes between 0.9933 and -0.9407  and returns to 0 
+		// does like 5 shakes, think of shaking the angle of a tree when player bumps into it
+		// ( ( cos( x * 3.1415 ) + 1 ) * 0.5 )  * sin( x * 30.0 )
+		struct DimishingShakeBack : public IEasingFunc {
+			virtual float f( float x );
+		};
+
+		// Shakes between 1.102 and -0.7851 and goes to 1
+		// same as DimishingShakeBack but actually ends up at the target
+		// does like 5 shakes, don't really know what to use this for, but 
+		// I'm sure I'll figure something out
+		// ( ( cos( x * 3.1415 ) + 1 ) * 0.5 )  * sin( x * 30.0 ) + x
+		struct DimishingShake : public IEasingFunc {
+			virtual float f( float x );
+		};
+
+		// A function that goes to -0.1 and then to 1.0, like taking a bit of speed before launching
+		// ((pow(x+0.17f, 1.3f) * sin(1.f/(x+0.17f))) * 1.043f) + 0.035f
+		struct LittleBackAndForth : public IEasingFunc {
+			virtual float f( float x );
+		};
+
+		// ----- 
 
 		// Randomf, just gives you a ceng::math::Randomf(0,1) as a result
 		struct Randomf : public IEasingFunc {
@@ -31,9 +59,12 @@ class PetriHacks {
 
 		//----------------
 
-		static SinGoTo2AndBack	sinGoTo2AndBack;
-		static Randomf			randomf;
-		static BooleanRandom	booleanRandom;
+		static SinGoTo2AndBack		sinGoTo2AndBack;
+		static DimishingShakeBack	dimishingShakeBack;
+		static DimishingShakeBack	dimishingShake;
+		static LittleBackAndForth	littleBackAndForth;
+		static Randomf				randomf;
+		static BooleanRandom		booleanRandom;
 };
 
 } // end of namespace easing
