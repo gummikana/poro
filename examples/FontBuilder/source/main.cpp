8d4b68af50d3fac90c11645121c80608e4c1ca8a
Petri Purho <petri.purho@gmail.com>
2014-01-09 16:47:53 +0200

    Examples - FontBuilder - fixed a few bugs and made it more roboust

--- examples/FontBuilder/source/main.cpp
+++ examples/FontBuilder/source/main.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 #include <sstream>
 
-#include "../../../source/game_utils/font/cfont.h"
+#include "../../../source/utils/string/string.h"
+#include "../../../source/utils/xml/cxml.h"
 #include "../../../source/utils/vector_utils/vector_utils.h"
 #include "../../../source/utils/filesystem/filesystem.h"
+#include "../../../source/game_utils/font/cfont.h"
 
 #define STB_TRUETYPE_IMPLEMENTATION
 #include "stb/stb_truetype.h"
@@ -36,29 +38,53 @@ std::string GetArgumentParam( const std::string& argument_name, const std::vecto
 	return default_value;
 }
 
+std::string GetArgumentParam( int i, const std::vector< std::string >& args, const std::string& default_value = "" )
+{
+	if( args.size() <= i ) 
+	{
+		return default_value;
+	}
+
+	return args[ i ];
+}
+
 //-----------------------------------------------------------------------------
+unsigned char ttf_buffer[1<<25];
+
+const int width = 4096;
+const int height = 20480;
+
+unsigned char bmap[ width*height ];
 
 void CreateFont(
 	const std::string& ttf_file,
 	float size,
 	const std::string& xml_filename,
-	const std::string& texture_file,
-	int width,
-	int height )
+	const std::string& texture_file )
 {
-	unsigned char ttf_buffer[1<<25];
-	unsigned char* bmap = (unsigned char*)malloc(width*height);
-	
-	fread(ttf_buffer, 1, 1<<25, fopen(ttf_file.c_str(), "rb"));
+	memset( bmap, 0, width*height );
+	memset( ttf_buffer, 0, 1 << 25 );
+	FILE* fptr = fopen(ttf_file.c_str(), "rb");
 
-	stbtt_bakedchar g_cdata[96]; // ASCII 32..126 is 95 glyphs
-	stbtt_BakeFontBitmap( ttf_buffer, 0, size, bmap, width, height, 32, 96, g_cdata);
+	if( fptr == NULL ) 
+	{
+		std::cout << "Error reading file: " << ttf_file << std::endl;
+		return;
+	}
+
+	fread(ttf_buffer, 1, 1<<25, fptr);
+
+	stbtt_bakedchar g_cdata[95]; // ASCII 32..126 is 95 glyphs
+	stbtt_BakeFontBitmap( ttf_buffer, 0, size, bmap, width, height, 32, 95, g_cdata);
 
 	CFont* font = new CFont;
 	font->SetLineHeight( size );
 	font->SetTextureFilename( texture_file );
 	
-	for( int i = 0; i < 96; ++i ) 
+	int tsize_x = 0;
+	int tsize_y = 0;
+
+	for( int i = 0; i < 95; ++i ) 
 	{
 		font->SetCharQuad( i + 32, 
 			new CFont::CharQuad( 
@@ -71,18 +97,24 @@ void CreateFont(
 					g_cdata[ i ].xoff, 
 					g_cdata[ i ].yoff ),
 				g_cdata[ i ].xadvance ) );
+
+		if( g_cdata[ i ].x1 > tsize_x ) tsize_x = g_cdata[ i ].x1;
+		if( g_cdata[ i ].y1 > tsize_y ) tsize_y = g_cdata[ i ].y1;
 	}
 
 	// int result = poro::ImageSave( icon_file.c_str(), w, h, 3, pixels, w * 3 );
 
+	tsize_x += 1;
+	tsize_y += 1;
+
 	// convert this into a 32bit image
 	unsigned char* pixels = (unsigned char*)malloc(width*height*4);
 
-	for( int y = 0; y < height; ++y )
+	for( int y = 0; y < tsize_y; ++y )
 	{
-		for( int x = 0; x < width; ++x )
+		for( int x = 0; x < tsize_x; ++x )
 		{
-			int p = ( 4 * x + 4 * width * y);
+			int p = ( 4 * x + 4 * tsize_x * y);
 			// int p2 = (x + (pos_x * 3)) + ( 3 * width * ( y + pos_y));
 			// int p1 = ( x + w * y )
 			pixels[ p + 0 ] = 255;
@@ -92,8 +124,13 @@ void CreateFont(
 		}
 	}
 
-	stbi_write_png( texture_file.c_str(), width, height, 4, pixels, width * 4 );
+	std::cout << "Creating file: " << texture_file << " ... ";
+	stbi_write_png( texture_file.c_str(), tsize_x, tsize_y, 4, pixels, tsize_x * 4 );
+	std::cout << "Done" << std::endl;
+	
+	std::cout << "Creating file: " << xml_filename << " ... ";
 	ceng::XmlSaveToFile( *font, xml_filename, "Font" );
+	std::cout << "Done" << std::endl;
 }
 // #endif
 
@@ -101,30 +138,35 @@ int main( int inargc, char* inargs[] )
 {
 	std::vector< std::string > args = ceng::ArgsToVector( inargc, inargs );
 
-	std::string font_file = GetArgumentParam( "-font", args );
+	std::string font_file = GetArgumentParam( 0, args );
+
+	// TEST CASE
+	// CreateFont( "GARA.ttf", 512.f, "gara.xml", "gara.png" );
 
 	if( font_file.empty() ) 
 	{
-		std::cout << "Font builder creates xml & png for poro, from a given ttf -file. Parameters are: " << std::endl
-		<< "\t -font path_to_ttf_file.ttf " << std::endl
-		<< "\t -size 16 " << std::endl
+		std::cout << "Font builder creates xml & png for poro, from a given ttf -file. Usage: " << std::endl
+		<< "font_build.exe times.ttf 18" << std::endl
+		<< "\t this will create data/fonts/times_18.xml and data/fonts/times_18.png files" << std::endl
+		<< "\t default size is 16" << std::endl
+		<< std::endl
+		<< "for more options: " << std::endl
 		<< "\t -xml path_to_xml_file.xml " << std::endl
-		<< "\t -texture path_to_texture_file.png " << std::endl
-		<< "\t -w 128 " << std::endl
-		<< "\t -h 128 " << std::endl;
+		<< "\t -texture path_to_texture_file.png " << std::endl;
 		return 0;
 	}
 
 	std::string font_name = ceng::GetFilenameWithoutExtension( font_file );
+	float size = ceng::CastFromString< float >( GetArgumentParam( 1, args, "16" ) );
+	
+	if( size <= 0 ) size = 16;
 
-	int width = ceng::CastFromString< int >( GetArgumentParam( "-w", args, "128" ) );
-	int height = ceng::CastFromString< int >( GetArgumentParam( "-h", args, "128" ) );
-	float size = ceng::CastFromString< float >( GetArgumentParam( "-size", args, "16" ) );
 	font_name = "data/fonts/" + font_name + "_" + ceng::CastToString( size );
 	std::string texture_filename = GetArgumentParam( "-texture", args, font_name + ".png" );
 	std::string xml_filename = GetArgumentParam( "-xml", args, font_name + ".xml" );
 
-	CreateFont( font_file, size, xml_filename, texture_filename, width, height );
+	CreateFont( font_file, size, xml_filename, texture_filename );
 
+	std::cout << "Success" << std::endl;
 	return 0;
 }
