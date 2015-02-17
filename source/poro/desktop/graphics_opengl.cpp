64a8fd8066cdb60327eda0518156c6d7e6fd438b
Petri Purho <petri.purho@gmail.com>
2015-02-17 11:37:56 +0200

    desktop - graphics_opengl.cpp - SDL_GL_SetAttribute(SDL_GL_RED_SIZE, 5) to SDL_GL_RED_SIZE, 8

--- source/poro/desktop/graphics_opengl.cpp
+++ source/poro/desktop/graphics_opengl.cpp
@@ -748,9 +748,9 @@ void GraphicsOpenGL::ResetWindow()
 		}
 	}
 
-	SDL_GL_SetAttribute(SDL_GL_RED_SIZE, 5);
-	SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE, 5);
-	SDL_GL_SetAttribute(SDL_GL_BLUE_SIZE, 5);
+	SDL_GL_SetAttribute(SDL_GL_RED_SIZE, 8);
+	SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE, 8);
+	SDL_GL_SetAttribute(SDL_GL_BLUE_SIZE, 8);
 	SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 16);
 	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
 	SDL_GL_SetAttribute(SDL_GL_ALPHA_SIZE, 8);
