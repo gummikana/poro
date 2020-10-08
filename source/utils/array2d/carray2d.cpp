9932066dd446bae1cbe3e660bdc6e18e7a376502
Petri Purho <petri.purho@gmail.com>
2020-10-08 14:02:44 +0300

    array2d - added #ifdefs for detecting leaked CArray2Ds and exposed the data

--- source/utils/array2d/carray2d.cpp
+++ source/utils/array2d/carray2d.cpp
@@ -23,4 +23,63 @@
 
 namespace ceng {
 
+#ifdef DEBUG_ARRAY2D_MEM_LEAKS
+
+
+AtomicInteger::AtomicInteger()
+{
+	Set( 0 );
 }
+
+AtomicInteger::AtomicInteger(int initial_value)
+{
+	Set( initial_value );
+}
+
+AtomicInteger::AtomicInteger( const AtomicInteger& other )
+{
+	Set( other.Get() );
+}
+
+void AtomicInteger::operator+=( int value )
+{
+	mValue.fetch_add( value );
+}
+
+void AtomicInteger::operator-=( int value )
+{
+	mValue.fetch_add( -value );
+}
+
+int AtomicInteger::operator++(int)
+{
+	return mValue.fetch_add( 1 );
+}
+
+int AtomicInteger::operator--(int)
+{
+	return mValue.fetch_add( -1 );
+}
+
+
+int AtomicInteger::Get() const
+{
+	return mValue.load();
+}
+
+void AtomicInteger::Set( int value )
+{
+	mValue.store( value );
+}
+
+AtomicInteger& AtomicInteger::operator =( const AtomicInteger& other )
+{
+	mValue.store( other.mValue.load() );
+	return *this;
+}
+
+AtomicInteger CArray2DLeaks::mCount = 0;
+
+#endif
+
+};
