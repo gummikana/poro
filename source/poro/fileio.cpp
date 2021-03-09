821ebf96bcf1b5c42a34d48cbe2ce5762059e87d
Olli Harjola <oharjola@gmail.com>
2021-03-09 14:18:15 +0200

    FIX - file write buffer could be leaked in case file couldn't be opened

--- source/poro/fileio.cpp
+++ source/poro/fileio.cpp
@@ -122,7 +122,9 @@ namespace platform_impl
 
 		~StreamInternal() override
 		{
-			poro_assert( mWriteBuffer == NULL );
+			if ( mWriteBuffer )
+				free( mWriteBuffer );
+			mWriteBuffer = NULL;
 		}
 
 		StreamStatus::Enum Read( char* out_buffer, u32 buffer_capacity_bytes, u32* out_bytes_read ) override
